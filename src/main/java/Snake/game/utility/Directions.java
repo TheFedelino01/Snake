c4175c8c5021cd72205c9978774972ffb3594f20
TheFedelino01 <federico.saccani01@gmail.com>
2019-08-28 10:32:05 +0200

    Aggiunti commenti classe Directions

--- src/main/java/Snake/game/utility/Directions.java
+++ src/main/java/Snake/game/utility/Directions.java
@@ -6,8 +6,11 @@
 package Snake.game.utility;
 
 /**
- *
- * @author Federico Saccani
+ @author  Saccani Federico, federico.saccani01@gmail.com
+ @version 1.0
+ */
+/**
+ La classe rappresenta le possibili direzioni che potranno essere effettuate dal serpente
  */
 public enum Directions {
     SOPRA, SOTTO, DESTRA, SINISTRA, NONE;
